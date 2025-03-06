62a4032bb3593034b0149a72d788bcf09323f227
ThanhhhNha <ngocthanhnhakhoa0206@gmail.com>
2025-03-06 10:10:19 +0700

    User

--- VietTravel/VietTravel/Areas/Admin/Controllers/UsersController.cs
+++ VietTravel/VietTravel/Areas/Admin/Controllers/UsersController.cs
@@ -1,37 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿using System.Net;
 using System.Web.Mvc;
 using VietTravel.Models;
+using VietTravel.Repositories;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace VietTravel.Areas.Admin.Controllers
 {
+    ///Repository Pattern : Tách biệt giữa logic truy cập dữ liệu và Controller.Dễ dàng test hơn vì có thể mock IUserRepository. Dễ dàng bảo trì và mở rộng hệ thống.
     public class UsersController : Controller
     {
-        private TravelVNEntities db = new TravelVNEntities();
+        private readonly IUserRepository _userRepository;
+
+        // Inject IUserRepository thông qua constructor
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
 
         // GET: Admin/Users
         public ActionResult Index()
         {
-            return View(db.Users.ToList());
+            var users = _userRepository.GetAll();
+            return View(users);
         }
 
         // GET: Admin/Users/Details/5
         public ActionResult Details(string id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
+
+            var user = _userRepository.GetById(id);
             if (user == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(user);
         }
 
@@ -42,19 +44,15 @@ namespace VietTravel.Areas.Admin.Controllers
         }
 
         // POST: Admin/Users/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "MaUser,TenUser,Username,Password,Email,DienThoai")] User user)
+        public ActionResult Create(User user)
         {
             if (ModelState.IsValid)
             {
-                db.Users.Add(user);
-                db.SaveChanges();
+                _userRepository.Add(user);
                 return RedirectToAction("Index");
             }
-
             return View(user);
         }
 
@@ -62,28 +60,23 @@ namespace VietTravel.Areas.Admin.Controllers
         public ActionResult Edit(string id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
+
+            var user = _userRepository.GetById(id);
             if (user == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(user);
         }
 
         // POST: Admin/Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "MaUser,TenUser,Username,Password,Email,DienThoai")] User user)
+        public ActionResult Edit(User user)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(user).State = EntityState.Modified;
-                db.SaveChanges();
+                _userRepository.Update(user);
                 return RedirectToAction("Index");
             }
             return View(user);
@@ -93,14 +86,12 @@ namespace VietTravel.Areas.Admin.Controllers
         public ActionResult Delete(string id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
+
+            var user = _userRepository.GetById(id);
             if (user == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(user);
         }
 
@@ -109,19 +100,136 @@ namespace VietTravel.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(string id)
         {
-            User user = db.Users.Find(id);
-            db.Users.Remove(user);
-            db.SaveChanges();
+            _userRepository.Delete(id);
             return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
+
+//using System;
+//using System.Collections.Generic;
+//using System.Data;
+//using System.Data.Entity;
+//using System.Linq;
+//using System.Net;
+//using System.Web;
+//using System.Web.Mvc;
+//using VietTravel.Models;
+
+//namespace VietTravel.Areas.Admin.Controllers
+//{
+//    public class UsersController : Controller
+//    {
+//        private TravelVNEntities db = new TravelVNEntities();
+
+//        // GET: Admin/Users
+//        public ActionResult Index()
+//        {
+//            return View(db.Users.ToList());
+//        }
+
+//        // GET: Admin/Users/Details/5
+//        public ActionResult Details(string id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            User user = db.Users.Find(id);
+//            if (user == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(user);
+//        }
+
+//        // GET: Admin/Users/Create
+//        public ActionResult Create()
+//        {
+//            return View();
+//        }
+
+//        // POST: Admin/Users/Create
+//        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+//        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Create([Bind(Include = "MaUser,TenUser,Username,Password,Email,DienThoai")] User user)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.Users.Add(user);
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+
+//            return View(user);
+//        }
+
+//        // GET: Admin/Users/Edit/5
+//        public ActionResult Edit(string id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            User user = db.Users.Find(id);
+//            if (user == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(user);
+//        }
+
+//        // POST: Admin/Users/Edit/5
+//        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+//        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Edit([Bind(Include = "MaUser,TenUser,Username,Password,Email,DienThoai")] User user)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.Entry(user).State = EntityState.Modified;
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+//            return View(user);
+//        }
+
+//        // GET: Admin/Users/Delete/5
+//        public ActionResult Delete(string id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            User user = db.Users.Find(id);
+//            if (user == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(user);
+//        }
+
+//        // POST: Admin/Users/Delete/5
+//        [HttpPost, ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult DeleteConfirmed(string id)
+//        {
+//            User user = db.Users.Find(id);
+//            db.Users.Remove(user);
+//            db.SaveChanges();
+//            return RedirectToAction("Index");
+//        }
+
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing)
+//            {
+//                db.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+//    }
+//}
---
+++ VietTravel/VietTravel/Repositories/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VietTravel.Models;
+
+namespace VietTravel.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly TravelVNEntities _context;
+
+        public UserRepository()
+        {
+            _context = new TravelVNEntities();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetById(string id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public void Add(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void Update(User user)
+        {
+            _context.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
